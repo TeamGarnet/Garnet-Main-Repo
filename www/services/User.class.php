78ee060f01e8b2549256a6744038417b644d2060
Brianna Jones <bfj5889@g.rit.edu>
2018-01-28 04:05:53 -0500

    Model and Service Stubs
    Majority of the classes needed for the entire project.

---
+++ www/services/User.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Minaloc
+ * Date: 1/28/2018
+ * Time: 3:56 AM
+ */
+
+class User
+{
+
+}
---
+++ www/services/WarOperations.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Minaloc
+ * Date: 1/28/2018
+ * Time: 3:50 AM
+ */
+
+class WarOperations
+{
+
+}
