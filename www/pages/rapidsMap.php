3a7193dd6ccf39a5e7903f2beafaede0dd82c808
Brianna Jones <bfj5889@g.rit.edu>
2018-01-28 19:32:32 -0500

    Map Backend V1
    Testing pulling map data from server by coordinate type

--- www/pages/rapidsMap.php
+++ www/pages/rapidsMap.php
@@ -1,1 +1,9 @@
 <?php
+include '../services/DatabaseConnection.class.php';
+include '../services/MapOperations.class.php';
+getAllCoordinatesByType('Grave');
+?>
+
+<!DOCTYPE html>
+
+</DOCTYPE>
--- www/services/MapOperations.class.php
+++ www/services/MapOperations.class.php
@@ -1,6 +1,37 @@
 <?php
 
+//Collect an instance of the DB
+$db = Database::getInstance();
+$mysqli = $db->getConnection();
 
 class MapOperations {
+    public function setCoordinates () {
 
+    }
+
+    public function getCoordinates(){
+
+    }
+
+    public function getAllCoordinatesByType ($type) {
+        $map = [];
+        try {
+            $query = "SELECT * FROM Map WHERE type = :type";
+
+            $query->bindParam(':type', $type, PDO::PARAM_STR);
+            $query->execute();
+            $query->setFetchMode(PDO::FETCH_CLASS,"Map");
+            while ($row = $query->fetch()) {
+                $map[] = $row;
+            }
+            echo 'Here is the map ' . "\n";
+            echo var_dump($map);
+            return $map;
+
+        } catch(PDOException $e){
+            echo $e->getMessage();
+            exit();
+        }
+
+    }
 }
