812104dc2fa86a963b69291e6b9fc874d37a74e7
Jake Fanelli <jakefanelli@Jakes-MacBook-Pro.local>
2018-01-28 22:18:32 -0500

    Updated DB design and fake data

--- sql.sql
+++ sql.sql
@@ -8,74 +8,125 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 -- Schema mydb
 -- -----------------------------------------------------
 -- -----------------------------------------------------
--- Schema garnet
+-- Schema RapidsCemetery
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema garnet
+-- Schema RapidsCemetery
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `garnet` DEFAULT CHARACTER SET utf8 ;
-USE `garnet` ;
+CREATE SCHEMA IF NOT EXISTS `RapidsCemetery` DEFAULT CHARACTER SET utf8 ;
+USE `RapidsCemetery` ;
 
 -- -----------------------------------------------------
--- Table `garnet`.`war`
+-- Table `RapidsCemetery`.`Image`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `garnet`.`war` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL DEFAULT NULL,
-  `dateStarted` DATE NULL DEFAULT NULL,
-  `dateEnded` DATE NULL DEFAULT NULL,
-  `description` VARCHAR(45) NULL DEFAULT NULL,
-  `image` VARCHAR(45) NULL DEFAULT NULL,
+CREATE TABLE IF NOT EXISTS `RapidsCemetery`.`Image` (
+  `id` INT NOT NULL,
+  `imageURL` VARCHAR(150) NULL,
+  `imageDescription` VARCHAR(9000) NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `garnet`.`grave`
+-- Table `RapidsCemetery`.`War`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `garnet`.`grave` (
+CREATE TABLE IF NOT EXISTS `RapidsCemetery`.`War` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NULL DEFAULT NULL,
-  `birthDate` DATE NULL DEFAULT NULL,
-  `deathDate` DATE NULL DEFAULT NULL,
-  `description` VARCHAR(45) NULL DEFAULT NULL,
-  `image` VARCHAR(45) NULL DEFAULT NULL,
-  `coordsFirst` DECIMAL(9,6) NULL DEFAULT NULL,
-  `coordsSecond` DECIMAL(9,6) NULL DEFAULT NULL,
-  `war_id` INT NULL DEFAULT NULL,
-  `pointValue` INT NULL DEFAULT NULL,
-  `hint` VARCHAR(45) NULL DEFAULT NULL,
+  `dateStarted` DATE NULL DEFAULT NULL,
+  `dateEnded` DATE NULL DEFAULT NULL,
+  `description` VARCHAR(9000) NULL DEFAULT NULL,
+  `imageID` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_grave_war_idx` (`war_id` ASC),
-  CONSTRAINT `fk_grave_war`
-    FOREIGN KEY (`war_id`)
-    REFERENCES `garnet`.`war` (`id`)
+  INDEX `fk_War_Image1_idx` (`imageID` ASC),
+  CONSTRAINT `fk_War_Image1`
+    FOREIGN KEY (`imageID`)
+    REFERENCES `RapidsCemetery`.`Image` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `garnet`.`vegetation`
+-- Table `RapidsCemetery`.`Map`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `garnet`.`vegetation` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL DEFAULT NULL,
-  `description` VARCHAR(45) NULL DEFAULT NULL,
-  `image` VARCHAR(45) NULL DEFAULT NULL,
-  `coordsFirst` DECIMAL(9,6) NULL DEFAULT NULL,
-  `coordsSecond` DECIMAL(9,6) NULL,
-  `pointValue` INT NULL DEFAULT NULL,
-  `hint` VARCHAR(45) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`))
+CREATE TABLE IF NOT EXISTS `RapidsCemetery`.`Map` (
+  `id` INT NOT NULL,
+  `latitude` DECIMAL(9,6) NULL,
+  `longitude` DECIMAL(9,6) NULL,
+  `type` ENUM('Grave','NaturalHistory','HazardousArea'),  
+PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `garnet`.`contacts`
+-- Table `RapidsCemetery`.`Grave`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `garnet`.`contacts` (
+CREATE TABLE IF NOT EXISTS `RapidsCemetery`.`Grave` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL DEFAULT NULL,
+  `birthDate` DATE NULL DEFAULT NULL,
+  `deathDate` DATE NULL DEFAULT NULL,
+  `description` VARCHAR(9000) NULL DEFAULT NULL,
+  `warID` INT NULL DEFAULT NULL,
+  `pointValue` INT NULL DEFAULT NULL,
+  `hint` VARCHAR(45) NULL DEFAULT NULL,
+  `mapID` INT NOT NULL,
+  `imageID` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_grave_war_idx` (`warID` ASC),
+  INDEX `fk_Grave_Map1_idx` (`mapID` ASC),
+  INDEX `fk_Grave_Image1_idx` (`imageID` ASC),
+  CONSTRAINT `fk_grave_war`
+    FOREIGN KEY (`warID`)
+    REFERENCES `RapidsCemetery`.`War` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Grave_Map1`
+    FOREIGN KEY (`mapID`)
+    REFERENCES `RapidsCemetery`.`Map` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Grave_Image1`
+    FOREIGN KEY (`imageID`)
+    REFERENCES `RapidsCemetery`.`Image` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `RapidsCemetery`.`NaturalHistory`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `RapidsCemetery`.`NaturalHistory` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL DEFAULT NULL,
+  `description` VARCHAR(9000) NULL DEFAULT NULL,
+  `pointValue` INT NULL DEFAULT NULL,
+  `hint` VARCHAR(45) NULL DEFAULT NULL,
+  `imageID` INT NOT NULL,
+  `mapID` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_NaturalHistory_Image1_idx` (`imageID` ASC),
+  INDEX `fk_NaturalHistory_Map1_idx` (`mapID` ASC),
+  CONSTRAINT `fk_NaturalHistory_Image1`
+    FOREIGN KEY (`imageID`)
+    REFERENCES `RapidsCemetery`.`Image` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_NaturalHistory_Map1`
+    FOREIGN KEY (`mapID`)
+    REFERENCES `RapidsCemetery`.`Map` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `RapidsCemetery`.`Contact`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `RapidsCemetery`.`Contact` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NULL DEFAULT NULL,
   `email` VARCHAR(45) NULL DEFAULT NULL,
@@ -85,52 +136,57 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `garnet`.`events`
+-- Table `RapidsCemetery`.`Event`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `garnet`.`events` (
+CREATE TABLE IF NOT EXISTS `RapidsCemetery`.`Event` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NULL DEFAULT NULL,
-  `description` VARCHAR(45) NULL DEFAULT NULL,
+  `description` VARCHAR(9000) NULL DEFAULT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `garnet`.`hazardousArea`
+-- Table `RapidsCemetery`.`HazardousArea`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `garnet`.`hazardousArea` (
+CREATE TABLE IF NOT EXISTS `RapidsCemetery`.`HazardousArea` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `coordsFirst` DECIMAL(9,6) NULL,
-  `coordsSecond` DECIMAL(9,6) NULL,
-  `image` VARCHAR(45) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`))
+  `mapID` INT NOT NULL,
+  `description` VARCHAR(9000) NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_HazardousArea_Map1_idx` (`mapID` ASC),
+  CONSTRAINT `fk_HazardousArea_Map1`
+    FOREIGN KEY (`mapID`)
+    REFERENCES `RapidsCemetery`.`Map` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `garnet`.`permissions`
+-- Table `RapidsCemetery`.`Permission`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `garnet`.`permissions` (
+CREATE TABLE IF NOT EXISTS `RapidsCemetery`.`Permission` (
   `id` INT NOT NULL,
   `name` VARCHAR(45) NULL DEFAULT NULL,
-  `description` VARCHAR(45) NULL DEFAULT NULL,
+  `description` VARCHAR(9000) NULL DEFAULT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `garnet`.`user`
+-- Table `RapidsCemetery`.`User`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `garnet`.`user` (
+CREATE TABLE IF NOT EXISTS `RapidsCemetery`.`User` (
   `id` INT NOT NULL,
   `email` VARCHAR(45) NULL DEFAULT NULL,
   `password` VARCHAR(45) NULL DEFAULT NULL,
-  `permissions_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `permissions_id`),
-  INDEX `fk_user_permissions1_idx` (`permissions_id` ASC),
+  `permissionID` INT NOT NULL,
+  PRIMARY KEY (`id`, `permissionID`),
+  INDEX `fk_user_permissions1_idx` (`permissionID` ASC),
   CONSTRAINT `fk_user_permissions1`
-    FOREIGN KEY (`permissions_id`)
-    REFERENCES `garnet`.`permissions` (`id`)
+    FOREIGN KEY (`permissionID`)
+    REFERENCES `RapidsCemetery`.`Permission` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
