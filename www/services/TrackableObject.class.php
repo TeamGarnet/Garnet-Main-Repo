afabe39d97f647b6714552a32a3071fdcc7a8400
Brianna Jones <bfj5889@g.rit.edu>
2018-01-30 00:09:59 -0500

    Mapping Debugging V1

--- www/services/TrackableObject.class.php
+++ www/services/TrackableObject.class.php
@@ -2,25 +2,25 @@
 
 class TrackableObjectOperations {
 
-//    public function getAllMapData() {
-//        $map = [];
-//        try {
-//            $query = "select * from TrackableObject join GraveDetail on GraveDetail.idGraveDetail = TrackableObject.idGraveDetail;";
-//
-////            $query->bindParam(':type', $type, PDO::PARAM_STR);e
-//            echo "Executing Query";
-//            $query->execute();
-//            $query->setFetchMode(PDO::FETCH_CLASS,"TrackableObject");
-//            while ($row = $query->fetch()) {
-//                $map[] = $row;
-//            }
-//            echo 'Here is the map ' . "\n";
-//            echo var_dump($map);
-//            return $map;
-//
-//        } catch(PDOException $e){
-//            echo $e->getMessage();
-//            exit();
-//        }
-//    }
+    public function getAllMapData() {
+        $map = [];
+        try {
+            $query = "select * from TrackableObject join GraveDetail on GraveDetail.idGraveDetail = TrackableObject.idGraveDetail";
+
+//            $query->bindParam(':type', $type, PDO::PARAM_STR);e
+            echo "Executing Query";
+            $query->execute();
+            $query->setFetchMode(PDO::FETCH_CLASS,"TrackableObject");
+            while ($row = $query->fetch()) {
+                $map[] = $row;
+            }
+            echo 'Here is the map ' . "\n";
+            echo var_dump($map);
+            return $map;
+
+        } catch(PDOException $e){
+            echo $e->getMessage();
+            exit();
+        }
+    }
 }
