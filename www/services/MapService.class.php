ee350919b4e1587e376d86287bce3968dde4527e
Brianna Jones <bfj5889@g.rit.edu>
2018-02-03 17:00:42 -0500

    Map Creation V4
    #25

--- www/services/MapService.class.php
+++ www/services/MapService.class.php
@@ -38,7 +38,14 @@ class MapService {
          */
         foreach ($pinObjectsArray as $pin) {
             echo "<br>";
-            print_r($pin);
+            //print_r($pin);
+
+            
+            echo "<br>";
+            echo $pin.getIdTrackableObject();
+            echo "<br>";
+            echo $pin.getLongitude();
+
             $markerCode .= "var " .  $pin.getIdTrackableObject() . "marker = new google.maps.Marker({
             position: {lat: " . $pin.getLatitude() . ", lng: " . $pin.getLongitude() .
             "} map: map, title: '" . $pin.getName() . "'});";
