57f20048ccd39c1659194a0662d1ee2935f9b931
bfj5889 <bfj5889@g.rit.edu>
2018-02-24 21:48:02 -0500

    few

--- www/services/MapService.class.php
+++ www/services/MapService.class.php
@@ -125,9 +125,10 @@ class MapService {
     public function generateInfoWindowConfig($pin, $markerName) {
         $objectInfoString = "onclick=" . "'loadObjectInfo("
             . $pin -> getIdTrackableObject()
-            . "," . "'"
+            . ","
+            . '\''
             . $pin -> getTypeFilterName()
-            . "'" . ");" . "'";
+            . '\'' . ");" . '\'';
 
 
         $infoWindowContent = ' " ' . "<div id=" . "'infoWindow'><image src='"
