27d099908350bb5754db1e0b00b595c76125618c
bfj5889 <bfj5889@g.rit.edu>
2018-02-25 01:06:12 -0500

    Modal Frontend

--- www/js/MapScript.js
+++ www/js/MapScript.js
@@ -19,15 +19,3 @@ function resetFilters() {
     }
 }
 
-// // Get the modal
-// var modal = document.getElementById('myModal');
-//
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-//
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// }
--- www/pages/rapidsMap.php
+++ www/pages/rapidsMap.php
@@ -141,11 +141,7 @@ $filterBar = $mapData -> generateFilterBar();
             dataType:"text",
             success: function(data) {
                 console.log(data);
-                // When the user clicks the link, open the modal
-                modal.style.display = "block";
-                // When the user clicks on <span> (x), close the modal
-                span.onclick = function() {
-                    modal.style.display = "none";
+
                 }
             }
         });
