d1a22cc790c475200a5ed3e29c30c62ad2efdbed
bfj5889 <bfj5889@g.rit.edu>
2018-02-24 22:06:12 -0500

    vds

--- www/services/MapService.class.php
+++ www/services/MapService.class.php
@@ -126,7 +126,7 @@ class MapService {
         $objectInfoString = "onclick=" . "'loadObjectInfo("
             . $pin -> getIdTrackableObject()
             . ","
-            . $pin -> getTypeFilterNameWithQuote()
+            . json_encode($pin -> getTypeFilterName())
             . ");" . "'";
 
 
