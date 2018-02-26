001f132ba53a47ed7cbacf79059bc89f933a044c
bfj5889 <bfj5889@g.rit.edu>
2018-02-25 20:40:56 -0500

    Closing HREF

--- www/pages/trails.php
+++ www/pages/trails.php
@@ -52,7 +52,7 @@ $allTrailInfo = $trailService -> formatTrailLocationsInfo();
 
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <img class="center-block img-responsive" src="images/TrailMap.jpg" alt=""/>
+            <img class="center-block img-responsive" src="../images/TrailMap.jpg" alt=""/>
         </div>
     </div>
 
