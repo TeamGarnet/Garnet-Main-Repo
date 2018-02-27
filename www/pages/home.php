c3719934cbe76888e0021cca9fa7f244c6779998
bfj5889 <bfj5889@g.rit.edu>
2018-02-27 06:33:03 -0500

    General Fixing

--- www/pages/home.php
+++ www/pages/home.php
@@ -10,20 +10,20 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="/pages/css/thirdParty/font-awesome.css" type="text/css">
-    <link href="/pages/css/thirdParty/bootstrap.min.css" rel="stylesheet"/>
-    <link href="/pages/css/thirdParty/YouTubePopUp.css" rel="stylesheet">
-    <link href="/pages/css/thirdParty/imagehover.css" rel="stylesheet">
-    <link href="/pages/css/thirdParty/dropdoun.css" rel="stylesheet">
-    <link href="/pages/css/thirdParty/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="../pages/css/thirdParty/font-awesome.css" type="text/css">
+    <link href="../pages/css/thirdParty/bootstrap.min.css" rel="stylesheet"/>
+    <link href="../pages/css/thirdParty/YouTubePopUp.css" rel="stylesheet">
+    <link href="../pages/css/thirdParty/imagehover.css" rel="stylesheet">
+    <link href="../pages/css/thirdParty/dropdoun.css" rel="stylesheet">
+    <link href="../pages/css/thirdParty/style.css" rel="stylesheet">
     <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 
 
-    <link rel="apple-touch-icon" sizes="120x120" href="/pages/images/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/pages/images/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/pages/images/favicon/favicon-16x16.png">
-    <link rel="manifest" href="/pages/images/favicon/site.webmanifest">
-    <link rel="mask-icon" href="/pages/images/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="apple-touch-icon" sizes="120x120" href="../pages/images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../pages/images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../pages/images/favicon/favicon-16x16.png">
+    <link rel="manifest" href="../pages/images/favicon/site.webmanifest">
+    <link rel="mask-icon" href="../pages/images/favicon/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
 </head>
