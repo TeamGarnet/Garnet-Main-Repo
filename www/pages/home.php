0eec021e57f5aa57a21c3cdfc0aea10ac3462449
Dervent Wright <dow4964@rit.edu>
2018-03-10 19:41:52 -0500

    fb icon styling 3 - now looks and works as expected!

--- www/pages/home.php
+++ www/pages/home.php
@@ -74,10 +74,10 @@
                 <div class="social" style="float:left;">
                     <a href="https://www.facebook.com/RapidsCemeteryRochester/" target="_blank" style="color:#FFFFFF">
                         <i class="fab fa-facebook-f"></i></a>
-                    <span style="padding:10px;"></span>
+                    <!--<span style="padding:10px;"></span>
                     <i class="fab fa-instagram"></i>
                     <span style="padding:10px;"></span>
-                    <i class="fab fa-twitter"></i></div>
+                    <i class="fab fa-twitter"></i>--></div>
                 <div style="color:#fff; float:right; font-family: prozolibre; padding-top:10px;"><h5>19th Ward Community Association
                         --- 216 Thurston Road Rochester,NY 14619 | (585)328-6571 | 19thWard@19wca.org</h5></div>
             </footer>
