aa9f8ef0f2dc459857854eaa63abbfcea3e8d826
Dervent Wright <dow4964@rit.edu>
2018-03-14 16:25:49 -0400

    Admin logout (will test again when login session is done

--- www/pages/admin/logout.php
+++ www/pages/admin/logout.php
@@ -1,11 +1,10 @@
 <?php
     session_start();
     if(isset($_SESSION['idUser'])) {
-        echo "Your session is running " . $_SESSION['idUser'];
-        echo "But you are now being logged out!";
+        echo "Your session is running " . $_SESSION['idUser']; // This line will be removed, currently for testing only.
+        echo "But you are now being logged out!"; // This line will be removed, currently for testing only.
         unset($_SESSION['idUser']);
         session_destroy();
     }
-    
     header("Location: login.php");
     exit;
