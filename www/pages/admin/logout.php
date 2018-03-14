4c7197f1bfc2d11f38d643f08b38ec8510d004b0
Dervent Wright <dow4964@rit.edu>
2018-03-14 16:11:00 -0400

    Admin logout 2

--- www/pages/admin/logout.php
+++ www/pages/admin/logout.php
@@ -5,5 +5,8 @@
         unset($_SESSION['idUser']);
         session_destroy();
     }
+    else if(!isset($_SESSION['idUser'])) {
+        echo "No session for a user, but redirecting anyway"; // This line will be removed, currently for testing only.
+    }
     header("Location: login.php");
     exit;
