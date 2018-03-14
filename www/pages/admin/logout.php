b482b6723ef2a8f2ca3d0ebdb12d01e8dbdaec55
Dervent Wright <dow4964@rit.edu>
2018-03-14 16:12:44 -0400

    Admin logout 3

--- www/pages/admin/logout.php
+++ www/pages/admin/logout.php
@@ -1,12 +1,13 @@
 <?php
     session_start();
     if(isset($_SESSION['idUser'])) {
-        echo "You are being logged out!"; // This line will be removed, currently for testing only.
+        echo "Your session is running " . $_SESSION['idUser'];
+        echo "But you are now being logged out!";
         unset($_SESSION['idUser']);
         session_destroy();
     }
     else if(!isset($_SESSION['idUser'])) {
-        echo "No session for a user, but redirecting anyway"; // This line will be removed, currently for testing only.
+        echo "No session for a user, but redirecting anyway";
     }
     header("Location: login.php");
     exit;
