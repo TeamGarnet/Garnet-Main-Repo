bec0b2fb76719ac8084360b5f584ea8a8e349c72
Dervent Wright <dow4964@rit.edu>
2018-03-14 16:42:04 -0400

    Admin login issue fixed; clearing test lines for admin logout to see if it will work

--- www/pages/admin/logout.php
+++ www/pages/admin/logout.php
@@ -1,8 +1,6 @@
 <?php
     session_start();
     if(isset($_SESSION['idUser'])) {
-        echo "Your session is running " . $_SESSION['idUser']; // This line will be removed, currently for testing only.
-        echo "But you are now being logged out!"; // This line will be removed, currently for testing only.
         unset($_SESSION['idUser']);
         session_destroy();
     }
