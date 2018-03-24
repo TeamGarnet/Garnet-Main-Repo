16191a54fd36151b2f200f602bce7506a6708663
bfj5889 <bfj5889@g.rit.edu>
2018-03-24 17:59:09 -0400

    will pinrt_r work

--- www/services/GraveService.class.php
+++ www/services/GraveService.class.php
@@ -98,7 +98,7 @@ class GraveService extends TrackableObjectService {
                 . "</td></tr>"
             ;
         }
-        echo $html;
+        print_r($html);
         return $html;
     }
 
