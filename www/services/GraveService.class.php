133115ffbf4dedff52334e071dc90d133cbc14a8
bfj5889 <bfj5889@g.rit.edu>
2018-03-24 23:28:36 -0400

    idk

--- www/services/GraveService.class.php
+++ www/services/GraveService.class.php
@@ -1,5 +1,5 @@
 <?php
-include_once '../../data/GraveObjectData.class.php';
+include_once '../data/GraveObjectData.class.php';
 include_once '../../models/Grave.class.php';
 include_once 'TrackableObjectService.class.php';
 
