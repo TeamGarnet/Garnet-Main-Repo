aaa19d124036d37be80b22f0c6b2dfb78218acf6
bfj5889 <bfj5889@g.rit.edu>
2018-03-26 19:58:00 -0400

    Why you not take the param

--- www/ajaxCalls.php
+++ www/ajaxCalls.php
@@ -2,7 +2,7 @@
 include_once 'services/MapService.class.php';
 include_once 'services/GraveService.class.php';
 
-echo("request type: " . $_SERVER['REQUEST_METHOD']);
+echo("request type: " . $_SERVER['REQUEST_METHOD'] . "<br>");
 echo("do we have a param: " . !empty($_POST['deleteGrave']));
 if(isset($_GET['getMapCardInfoID'])) {
     echo("In getMapcard");
