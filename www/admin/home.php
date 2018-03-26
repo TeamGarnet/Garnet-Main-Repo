63e4c814a4d80667a9491bd253e5771f9e0681ab
Dervent Wright <dow4964@rit.edu>
2018-03-26 11:10:43 -0400

    html for update modal

--- www/admin/home.php
+++ www/admin/home.php
@@ -464,7 +464,7 @@ $eventService->deleteEventEntry("5");
 
     function updateGrave(id) {
         var input = null;
-        $('th').each(function(index) {
+        $('#grave th').each(function(index) {
             var attribute = $(this).text().replace('\s*', '');
             var labelText =  $( this ).text() + ':';
 
