7ab104e50df40b305e33b768293028abd4c0c073
bfj5889 <bfj5889@g.rit.edu>
2018-03-26 19:43:22 -0400

    We hitting something XD

--- www/ajaxCalls.php
+++ www/ajaxCalls.php
@@ -2,13 +2,18 @@
 include_once 'services/MapService.class.php';
 include_once 'services/GraveService.class.php';
 
+
 if(isset($_GET['getMapCardInfoID'])) {
+    echo("In getMapcard");
     $mapService = new MapService();
     $mapService -> getMapCardInfo($_GET['getMapCardInfoID']);
     unset($_GET['getMapCardInfoID']);
 } else if (isset($_GET['deleteGrave'])) {
+    echo("In deleteGrave");
     $graveService = new GraveService();
+    echo("About to run delete grave...");
     $graveService ->deleteGraveEntry($_GET['deleteGrave']);
+    echo("unsetting delteGrave");
     unset($_GET['deleteGrave']);
 }
 else if (isset($_POST['updateGraveEntry'])) {
