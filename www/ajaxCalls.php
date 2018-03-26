32fab86fba8028cceac077656094acfb08d2fcfc
bfj5889 <bfj5889@g.rit.edu>
2018-03-26 19:52:15 -0400

    lol so we not doing post now?

--- www/ajaxCalls.php
+++ www/ajaxCalls.php
@@ -7,7 +7,7 @@ if(isset($_GET['getMapCardInfoID'])) {
     $mapService = new MapService();
     $mapService -> getMapCardInfo($_GET['getMapCardInfoID']);
     unset($_GET['getMapCardInfoID']);
-} else if (isset($_POST['deleteGrave'])) {
+} else if ($_SERVER['REQUEST_METHOD'] == "POST" && !empty($_POST['deleteGrave'])) {
     $graveService = new GraveService();
     $status = $graveService ->deleteGraveEntry($_GET['deleteGrave']);
     echo("Status: " . $status);
