d04ec65254d35e3f1548c5316608eebd5ec8f879
Dervent Wright <dow4964@rit.edu>
2018-03-26 15:25:11 -0400

    Updated styling of update modal

--- www/admin/home.php
+++ www/admin/home.php
@@ -137,7 +137,7 @@ $eventService->deleteEventEntry("5");
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="updateModalTitle">Update Object</h5>
+                <h3 class="modal-title" id="updateModalTitle">Update Object</h3>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
