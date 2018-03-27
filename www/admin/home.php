99e57366ff214aae33fc4a9073c1067217201adb
bfj5889 <bfj5889@g.rit.edu>
2018-03-26 22:07:50 -0400

    That moment when you copy the wrong code

--- www/admin/home.php
+++ www/admin/home.php
@@ -382,9 +382,7 @@ $eventService->deleteEventEntry("5");
 </div>
 
 <!-- Small modal -->
-<button type="button" class="btn btn-primary message" id="message" data-toggle="modal" data-target=".bd-example-modal-sm">Small modal</button>
-
-<div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+<div class="modal fade bd-example-modal-sm message" id="message" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-sm">
         <div class="modal-content">
 
