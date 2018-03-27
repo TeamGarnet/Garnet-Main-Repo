b460548c1f79b492be7fdbd1d01b66e652db3a9c
bfj5889 <bfj5889@g.rit.edu>
2018-03-26 21:07:06 -0400

    I think my query is broken rip

--- www/ajaxCalls.php
+++ www/ajaxCalls.php
@@ -27,49 +27,49 @@ if ($_SERVER['REQUEST_METHOD'] == "POST" && $_POST['action'] == 'delete') {
         unset($_POST['deleteGrave']);
 
     } else if (!empty($_POST['deleteNH'])) {
-        echo("param status: " . $_POST['deleteNH'] . "<br>");
+        echo("param status deleteNH: " . $_POST['deleteNH'] . "<br>");
         $service = new NaturalHistoryService();
         $service ->deleteNaturalHistoryEntry($_POST['deleteNH']);
         unset($_POST['deleteNH']);
 
     } else if (!empty($_POST['deleteMisc'])) {
-        echo("param status: " . $_POST['deleteMisc'] . "<br>");
+        echo("param status deleteMisc: " . $_POST['deleteMisc'] . "<br>");
         $service = new MiscObjectService();
         $service -> deleteMiscObjectEntry($_POST['deleteMisc']);
         unset($_POST['deleteMisc']);
 
     } else if (!empty($_POST['deleteType'])) {
-        echo("param status: " . $_POST['deleteType'] . "<br>");
+        echo("param status deleteType: " . $_POST['deleteType'] . "<br>");
         $service = new TypeFilterService();
         $service ->deleteTypeFilterEntry($_POST['deleteType']);
         unset($_POST['deleteType']);
 
     } else if (!empty($_POST['deleteHistoricFilter'])) {
-        echo("param status: " . $_POST['deleteHistoricFilter'] . "<br>");
+        echo("param status deleteHistoricFilter: " . $_POST['deleteHistoricFilter'] . "<br>");
         $service = new HistoricFilterService();
         $service ->deleteHistoricFilterEntry($_POST['deleteHistoricFilter']);
         unset($_POST['deleteHistoricFilter']);
 
     } else if (!empty($_POST['deleteFAQ'])) {
-        echo("param status: " . $_POST['deleteFAQ'] . "<br>");
+        echo("param status deleteFAQ: " . $_POST['deleteFAQ'] . "<br>");
         $service = new FAQService();
         $service ->deleteFAQEntry($_POST['deleteFAQ']);
         unset($_POST['deleteFAQ']);
 
     } else if (!empty($_POST['deleteLocation'])) {
-        echo("param status: " . $_POST['deleteLocation'] . "<br>");
+        echo("param status deleteLocation: " . $_POST['deleteLocation'] . "<br>");
         $service = new WiderAreaMapService();
         $service ->deleteWiderAreaMapEntry($_POST['deleteLocation']);
         unset($_POST['deleteLocation']);
 
     } else if (!empty($_POST['deleteContact'])) {
-        echo("param status: " . $_POST['deleteContact'] . "<br>");
+        echo("param status deleteContact: " . $_POST['deleteContact'] . "<br>");
         $service = new ContactService();
         $service ->deleteContactEntry($_POST['deleteContact']);
         unset($_POST['deleteContact']);
 
     } else if (!empty($_POST['deleteEvent'])) {
-        echo("param status: " . $_POST['deleteEvent'] . "<br>");
+        echo("param status deleteEvent: " . $_POST['deleteEvent'] . "<br>");
         $service = new EventService();
         $service ->deleteEventEntry($_POST['deleteEvent']);
         unset($_POST['deleteEvent']);
