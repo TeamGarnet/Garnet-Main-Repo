a380274ee1b70a29711b4cdf05b9bd87a161d318
bfj5889 <bfj5889@g.rit.edu>
2018-03-28 21:08:19 -0400

    Should porb import the file

--- www/admin/home.php
+++ www/admin/home.php
@@ -104,8 +104,7 @@ $eventService->deleteEventEntry("5");
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css"/>
-
-
+    <script src="../js/AdminHome.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
