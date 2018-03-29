87a34101f5f08b43be96179c45bbca023dfd3b72
JakeFanelli <31941477+JakeFanelli@users.noreply.github.com>
2018-03-29 06:31:17 -0400

    :)

--- www/home.php
+++ www/home.php
@@ -93,27 +93,6 @@
             </footer>
         </div>-->
     </div>
-
 </section>
-
-
-<!-- nav bar on tablet and computer-->
-
-
-<!--<script src='js/YouTubePopUp.jquery.js'></script>
-<script>jQuery(function () {
-        jQuery("a.bla-1").YouTubePopUp();
-        jQuery("a.bla-2").YouTubePopUp({autoplay: 0}); // Disable autoplay
-    });</script>-->
-
-
-<!-- search -->
-<!--<script src="js/jquery.nice-select.min.js"></script>-->
-<!---<script>
-(function($) {$.fn.parallax = function(options) { var windowHeight = $(window).height();    var settings = $.extend({speed        : 0.15}, options);return this.each( function() {var $this = $(this);$(document).scroll(function(){
-var scrollTop = $(window).scrollTop();var offset = $this.offset().top;var height = $this.outerHeight();if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {return;}var yBgPosition = Math.round((offset - scrollTop) * settings.speed);$this.css('background-position', 'center ' + yBgPosition + 'px');});});}}(jQuery));$('#medal_section,#medal_section1,#futer_heand,#knowl_gift,#conatct,#software_section,#traning_section').parallax({speed :	0.15});$('').parallax({speed :	0.25});
-</script>--->
 </body>
-
-<!-- Mirrored from cmsdevelop.net/sharuk/doodoo/ by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 28 Dec 2017 08:45:39 GMT -->
 </html>
