5d1d869ad4761190fd9cc6052c80b5f65365496e
JakeFanelli <31941477+JakeFanelli@users.noreply.github.com>
2018-03-29 03:11:56 -0400

    moving explore button up a little bit

--- www/home.php
+++ www/home.php
@@ -63,7 +63,7 @@
                             <p style=" font-family: ProzaLibre;">Unlock the history of the little known Rochester
                                 cemetery in this<br> interactive history experience</p>
                             <div class="top_btn">
-                                    <a href="rapidsMap.php"<button type="button" class="btn donate">explore map</button></a>
+                                    <a href="rapidsMap.php"<button type="button" class="btn donate" id="explore">explore map</button></a>
                             </div>
                         </div>
                     </div>
