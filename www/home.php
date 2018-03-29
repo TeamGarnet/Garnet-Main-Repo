641579704b9e1a200e11383f848630d386653979
JakeFanelli <31941477+JakeFanelli@users.noreply.github.com>
2018-03-29 05:01:41 -0400

    changing order

--- www/home.php
+++ www/home.php
@@ -9,8 +9,8 @@
     <title>Rapids Cemetery</title>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	
     <!-- Bootstrap -->
     <link rel="stylesheet" href="css/thirdParty/font-awesome.css" type="text/css">
     <link href="css/thirdParty/bootstrap.min.css" rel="stylesheet"/>
