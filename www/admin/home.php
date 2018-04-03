cf70e0a8781e5ed6e349fe475e269b3812f9f14d
JakeFanelli <31941477+JakeFanelli@users.noreply.github.com>
2018-04-03 02:13:33 -0400

    Revert "test with faq bc faq"
    This reverts commit 775cbb0b433b3c5fe0148735fa5009e0603a5c05.

--- www/admin/home.php
+++ www/admin/home.php
@@ -149,7 +149,7 @@ $eventService->deleteEventEntry("5");
 				<li><a href="#miscDiv" data-toggle="tab">Miscellaneous</a></li>
 				<li><a href="#typeDiv" data-toggle="tab">Type Filters</a></li>
 				<li><a href="#historicDiv" data-toggle="tab">Historic Filters</a></li>
-				<li><a href="#faq" data-toggle="tab">FAQ</a></li>
+				<li><a href="#faqDiv" data-toggle="tab">FAQ</a></li>
 				<li><a href="#widerLocationDiv" data-toggle="tab">Wider Area Locations</a></li>
 				<li><a href="#contactDiv" data-toggle="tab">Contacts</a></li>
 				<li><a href="#eventDiv" data-toggle="tab">Events</a></li>
