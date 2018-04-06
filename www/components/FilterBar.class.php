e19d2c056bf7bd411163c6f429f700c95ad0dd6a
JakeFanelli <31941477+JakeFanelli@users.noreply.github.com>
2018-04-06 15:06:57 -0400

    PLS?

--- www/components/FilterBar.class.php
+++ www/components/FilterBar.class.php
@@ -25,12 +25,12 @@ class FilterBar {
             if ($filterButton->getFilterName() != "No Historic Filter") {
                 $filterButtonCode = "<li><button type='button' class='btn btn-default' style='background-color: " . $filterButton -> getButtonColor() . "; border: 2px solid " . $filterButton -> getButtonColor() . " !important;'" .
                     "onclick=" . "\"refreshFilters('" . $filterButton -> getTable() . "'," . $filterButton -> getFilterID() . ")" . "\">" .
-                    $filterButton -> getFilterName() . "</button></li></ul>";
+                    $filterButton -> getFilterName() . "</button></li>";
 
                 $filterBarCode = $filterBarCode . $filterButtonCode;
             }
         }
-        $filterBarCode = $filterBarCode . "<ul class='nav navbar-nav navbar-right'><li><a href='#' onclick=" . "\"resetFilters()\">Reset Filters</a></li></ul></div></div></nav>";
+        $filterBarCode = $filterBarCode . "</ul><ul class='nav navbar-nav navbar-right'><li><a href='#' onclick=" . "\"resetFilters()\">Reset Filters</a></li></ul></div></div></nav>";
         $this -> setFilterBar($filterBarCode);
     }
 
