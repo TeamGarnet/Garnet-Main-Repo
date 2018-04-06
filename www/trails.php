70649fe378425777c0f170314cc9191e41fbfca0
JakeFanelli <31941477+JakeFanelli@users.noreply.github.com>
2018-04-06 11:44:47 -0400

    getting rid of it all

--- www/trails.php
+++ www/trails.php
@@ -123,20 +123,6 @@ $allMapPins = $widerAreaMapService -> generateMarkers();
 <div class="modal message" id="locationInfoModal" tabindex="-1" role="dialog">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
-            <div class="modal-header">
-				<div class="container">
-					<div class="row">
-						<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
-							<h5 class="modal-title" id="locationName">Location Information</h5>
-						</div>
-						<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-							<span aria-hidden="true">&times;</span>
-						</button>
-						</div>
-					</div>
-				</div>
-            </div>
             <div class="modal-body" id="locationInfoBody">
             </div>
             <div class="modal-footer">
