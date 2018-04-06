995646efa60ace9a82b0ee56f9c8fed5c763cf57
JakeFanelli <31941477+JakeFanelli@users.noreply.github.com>
2018-04-06 11:35:25 -0400

    pt1 update

--- www/trails.php
+++ www/trails.php
@@ -126,7 +126,7 @@ $allMapPins = $widerAreaMapService -> generateMarkers();
             <div class="modal-header">
 				<div class="container">
 					<div class="row">
-						<div class="col-xs-11 col-sm-11 col-md-11 col-lg-11">
+						<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
 							<h5 class="modal-title" id="locationName">Location Information</h5>
 						</div>
 						<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
