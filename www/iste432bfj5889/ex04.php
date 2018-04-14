fec310d057cc214433aab05cb68ee3754d46fc1d
bfj5889 <bfj5889@g.rit.edu>
2018-04-13 20:19:39 -0400

    V1

---
+++ www/iste432bfj5889/ex04.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Class: ${NAME}
+ * Date: 4/13/2018
+ * Description:
+ */
+function createHash($txt){
+    $salt = "myRand0msalt%";
+    return hash('sha256',$txt, $salt);
+}
+
+?>
+<!-- HTML -->
+<!DOCTYPE html>
+<html>
+
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+
+    <body>
+    <label for="txt">Hashing Example</label>
+    <input type="text" id="txt" name="txt" autocomplete="off" placeholder="Enter text"/>
+
+    <button onclick="createHash()" id="btn"> Generate Hash </button>
+    <input type="text" id="output" name="'output"/>
+
+    </body>
+</html>
+
+<!--- JS --->
+function createHash() {
+    $(document).ready(function() {
+        $('#btn').click(function() {
+            alert($('#txt').val());
+        });
+    });
+}
