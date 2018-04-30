24e43fe1e8a340f2ea4453e265796ee201d84673
bfj5889 <bfj5889@g.rit.edu>
2018-04-30 01:10:35 -0400

    Update Comments for where changes will be needed

--- www/trails.php
+++ www/trails.php
@@ -226,6 +226,7 @@ $allMapPins = $widerAreaMapService -> generateMarkers();
     }
 
 </script>
+<!------ UPDATE NEEDED:  Correct API Key for Javascript ------>
 <script async defer
         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA-sglJvUDWiUe_6Pe_sV9-SdtIvN_J-Vo&callback=initMap">
 </script>
