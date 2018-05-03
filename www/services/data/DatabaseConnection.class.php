93de86379e6bf623535f1539ee44ee0739a617e3
bfj5889 <bfj5889@g.rit.edu>
2018-05-03 13:25:02 -0400

    Put PWD Back

--- www/services/data/DatabaseConnection.class.php
+++ www/services/data/DatabaseConnection.class.php
@@ -41,8 +41,8 @@ class DatabaseConnection {
             /***** UPDATE NEEDED:  dsn, username, and password  *****/
             $dsn = 'mysql:host=localhost;port=3306;dbname=RapidsCemetery';
             $username = 'root';
-            $password = '';
-            //$password = '$peedingT1ckets4the$l0w!';
+            //$password = '';
+            $password = '$peedingT1ckets4the$l0w!';
             $options = array(
                 PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
             );
